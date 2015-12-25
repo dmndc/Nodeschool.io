fff2a7eb69389ebba9cf4cc80aef6cfb3e2a3d99
Dino <dinomandic@hotmail.com>
2015-12-25 23:42:50 +0100

    Change argument name

--- stream-adventure/http_server.js
+++ stream-adventure/http_server.js
@@ -2,13 +2,13 @@ var http = require('http');
 var through = require('through2');
 
 var server = http.createServer(function(req, res) {
-    if (req.method === 'POST') {
-        req.pipe(through(function (chunk, _, next) {
-            this.push(chunk.toString().toUpperCase());
-            next();
-        })).pipe(res);
-    }
-    else res.end('Send me a POST request\n');
+  if (req.method === 'POST') {
+    req.pipe(through(function (buffer, _, next) {
+      this.push(buffer.toString().toUpperCase());
+        next();
+      })).pipe(res);
+  }
+  else res.end('Send me a POST request\n');
 });
 
 server.listen(parseInt(process.argv[2]));
