bada8d12c908fa1ce4d67438736e80efd38f706f
Dino <dinomandic@hotmail.com>
2016-01-13 23:09:11 +0100

    add function

--- lololodash/everyone_is_min.js
+++ lololodash/everyone_is_min.js
@@ -6,5 +6,19 @@ module.exports = function(collection) {
     warm: []
   };
 
+  function check_temp (item) {
+    return item > 19;
+  }
+
+  _.forEach(item, function (town, townname) {
+
+    if (_.every(town, check_temp)) {
+        result.hot.push(townname);
+    } else if (_.some(town, check_temp)) {
+        result.warm.push(townname);
+    }
+
+});
+
   return result;
 };
