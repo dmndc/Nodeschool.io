f1604cc8efd48a6230b1e0316731509f4fcaf603
Dino <dinomandic@hotmail.com>
2016-01-30 22:33:54 +0100

    inital commit

---
+++ functional-javascript/reduce.js
@@ -0,0 +1,27 @@
+function countWords(inputWords) {
+  var results = {};
+  inputWords.reduce(function(prev, cur) {
+    if(!results[cur]) {
+      results[cur] = 1;
+    } else {
+      results[cur]++;
+    }
+  }, {})
+  return results;
+}
+
+module.exports = countWords;
+
+
+
+// Official solution
+
+function countWords(arr) {
+  return arr.reduce(function(countMap, word) {
+
+    countMap[word] = ++countMap[word] || 1 // increment or initialize to 1
+    return countMap
+  }, {}) // second argument to reduce initialises countMap to {}
+}
+
+module.exports = countWords
