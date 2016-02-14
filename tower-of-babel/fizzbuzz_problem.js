49d5103fbbf65df5543066b2b51f34d15653e52b
Dino <dinomandic@hotmail.com>
2016-02-14 23:51:54 +0100

    initial commit

--- tower-of-babel/fizzbuzz_problem.js
+++ tower-of-babel/fizzbuzz_problem.js
@@ -2,7 +2,7 @@ const max = process.argv[2];
 let FizzBuzz = {
   [Symbol.iterator]() {
     // here belongs the FizzBuzz logic
-    // Hint：
+
     // When it's finished you have to return an object
     // with the property `done: true` but before you
     // have to set `done: false`
